235aed26ca07d1809e085e7a1545eefc2fbe26ff
Squiremajor <cwperkins12@gmail.com>
2021-02-09 23:00:31 -0500

    Resize function now works! Colors are next

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let container = document.querySelector('.container')
 
-function makeGrid (rows = 16, columns = 16) {
+function makeGrid (rows, columns) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', columns);
     for (i=0; i<(rows * columns); i++) {
@@ -9,20 +9,30 @@ function makeGrid (rows = 16, columns = 16) {
         cell.addEventListener('mouseover', () => {
             cell.style.backgroundColor = 'black';
         });
+        console.log(i);
     }
     
 }
-makeGrid();
+makeGrid(16, 16); //Creates a Default grid of 256 squares.
 
 const resize = document.querySelector('.resize');
 resize.addEventListener('click', () => {
-    let newSize = window.prompt("Enter a new size", 16);
+    let newSize = window.prompt("Enter a new size");
     if (newSize > 100) {
         newSize = 100;
-    }
+    }    
     while (container.hasChildNodes()){
-        container.removeChild(container.firstChild());
+        container.removeChild(container.firstChild);
     }
     makeGrid(newSize, newSize);
 })
 
+const colorize = document.querySelector('.colorize');
+colorize.addEventListener('click', () => {
+    cell.addEventListener('mouseover', () => {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        cell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    })
+})
